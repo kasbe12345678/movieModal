1c366359175d6e0510d16617aa03513224583ca9
ashwini kasbe <ashwinikasbe99@gmail.com>
2022-10-05 17:03:24 +0530

    3rd commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,15 @@ let cl=console.log;
 const showmodalBtn=document.getElementById('showmodal');
 const backdrop=document.getElementById('backdrop');
 const mymodal=document.getElementById('mymodal');
+const movieinfo=document.getElementById('movieinfo');
 const myclose=Array.from(document.querySelectorAll('.myclose')); 
+const movieform=document.getElementById('movieform');
+const title=document.getElementById('title');
+const imageUrl=document.getElementById('imageUrl');
+const rating=document.getElementById('rating');
+
+
+let moviearr=[];
 
 //==================callback functions==========
 
@@ -16,13 +24,30 @@ const showmodalhandler = function(){
 
 
 }
-const hidemodalhandler=function(){
+const hidemodalhandler = function(){
     backdrop.classList.add('d-none');
      mymodal.classList.add('d-none');
         
 }
 
 
+const onmovieaddhandler = function(eve){        
+        eve.preventdefault();
+        //cl(eve.target);
+        let obj={
+            title:title.value,
+            imageUrl:imageUrl.value,
+            rating:rating.value,
+
+        }
+        cl(obj);
+        moviearr.push(obj);
+        templating(moviearr);
+
+       // eve.target.reset();
+       this.reset();
+       hidemodalhandler();
+}
 
 
 
@@ -35,5 +60,31 @@ myclose.forEach(function(ele){
     ele.addEventListener('click', hidemodalhandler)
 })
 
+movieform.addEventListener('submit', onmovieaddhandler);
+
+function templating(arr){
+    let result="";
+    arr.forEach(function(ele){
+        result+=`
+        <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <figure>
+                            <h3>${ele.title}</h3>
+                            <img src="${ele.imageUrl}" alt="">
+                            <figcaption>
+                                <p>
+                                ${ele.rating}
+                                    /5
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        `
+        movieinfo.innerHTML=result;
+    })
+}
 
 //==========function declaration===========
